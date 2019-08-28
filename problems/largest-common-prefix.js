20880ba05e58e5081790ec5bd4ee33ac1e0ba34c
klee2010 <klee2010@gmail.com>
2019-08-27 17:34:20 -0700

    divide and conquer approach for longest common prefix

--- problems/largest-common-prefix.js
+++ problems/largest-common-prefix.js
@@ -34,6 +34,55 @@ const LongestCommonPrefix = function(strs) {
     return prefix;
 };
 
+/**
+ * 
+ * Given an array of strings, the below functions will return the largest common prefix using a 
+ * Divide and Conquer approach.
+ * 
+ * The recurrence relation for this Divide and Conquer method is:
+ * 
+ * T(n) = 2 * T(n/2) + O(m)
+ * 
+ * Time Complexity: O(mn)
+ * Space Complexity: O(m * log n)
+ * 
+ */
+
+const LCP = (string1, string2) => {
+    let i = 0;
+    let j = 0;
+
+    while (i < string1.length && j < string2.length) {
+        if (string1[i] !== string2[j]) break;
+        i++;
+        j++;
+    }
+
+    return string1.substr(0, i);
+}
+
+const DCHelper = (strArray, low, high) => {
+    if (low > high) {
+        return '';
+    }
+
+    if (low === high) {
+        return strArray[low];
+    }
+
+    let mid = Math.floor((low + high) / 2);
+    let string1 = DCHelper(strArray, low, mid);
+    let string2 = DCHelper(strArray, mid+1, high);
+
+    return LCP(string1, string2);
+}
+
+const LongestCommonPrefix_DC = (strArray) => {
+    return DCHelper(strArray, 0, strArray.length - 1);
+}
+
+
 module.exports = {
     LongestCommonPrefix,
+    LongestCommonPrefix_DC,
 }
--- test/largest-common-prefix-test.js
+++ test/largest-common-prefix-test.js
@@ -1,7 +1,7 @@
 let assert = require('assert');
-let { LongestCommonPrefix } = require('../problems/largest-common-prefix');
+let { LongestCommonPrefix, LongestCommonPrefix_DC } = require('../problems/largest-common-prefix');
 
-describe('LargestCommonPrefix', function() {
+xdescribe('LargestCommonPrefix', function() {
     it('should return empty if no common prefix', function() {
         assert.equal(LongestCommonPrefix(["dog", "racecar", "car"]), "");
     });
@@ -11,5 +11,18 @@ describe('LargestCommonPrefix', function() {
     it('should return the largest common prefix', function() {
         assert.equal(LongestCommonPrefix(["a", "ab", "abc"]), "a");
         assert.equal(LongestCommonPrefix(["flower", "flow", "flight"]), "fl");
+    });
+});
+
+describe('LargestCommonPrefix - Divide and Conquer Method', function() {
+    xit('should return empty if no common prefix', function() {
+        assert.equal(LongestCommonPrefix_DC(["dog", "racecar", "car"]), "");
+    });
+    xit('should return the word if strArray is of length 1', function() {
+        assert.equal(LongestCommonPrefix_DC(["dog"]), "dog");
+    });
+    it('should return the largest common prefix', function() {
+        assert.equal(LongestCommonPrefix_DC(["a", "ab", "abc"]), "a");
+        assert.equal(LongestCommonPrefix_DC(["flower", "flow", "flight"]), "fl");
     });
 });
