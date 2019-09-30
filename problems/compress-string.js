04da98bc010b87bfa3f5de6318778b137cfb0533
klee2010 <klee2010@gmail.com>
2019-09-30 22:11:15 +0800

    added compres string problem

---
+++ problems/compress-string.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Given an array of characters, compress it. The length after compression must always be 
+ * smaller than or equal to the original array. Every element of the array should 
+ * be a character (not int) of length 1. After you are done modifying the input array in-place, 
+ * return the new length of the array.
+ * 
+ * @param {character[]} chars
+ * @return {number}
+ * 
+ */
+var compressString = function(chars) {
+    let count = 0;
+    let output = [];
+    for (let i=0; i<chars.length; i++) {
+        if (chars[i] !== chars[i+1]) {
+            output.push(chars[i]);
+            if (count > 1) {
+                output.push(count);
+            }
+            count = 0;
+        }
+        count++;
+    }
+    return output.length;
+}
+ 
+
+module.exports = {
+    compressString,
+}
---
+++ test/compress-string-test.js
@@ -0,0 +1,9 @@
+let assert = require('assert');
+let { compressString } = require('../problems/compress-string');
+
+describe('Compress String', function() {
+    it('compress a string with repeat characters', function() {
+      assert.equal(compressString(["a", "a", "a", "a"]), 2);
+      assert.equal(compressString(["a", "b", "b", "b", "c"]), 4);
+    });
+});
